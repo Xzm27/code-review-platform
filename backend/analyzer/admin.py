363bbe821e5333b09db61599f8e730883a5a38ed
root <root@xzm.localdomain>
2025-06-03 20:15:16 +0000

    Register CodeSubmission in Django admin

--- backend/analyzer/admin.py
+++ backend/analyzer/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import CodeSubmission
+
+@admin.register(CodeSubmission)
+class CodeSubmissionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'language', 'created_at')
+    list_filter = ('language', 'created_at')
+    search_fields = ['user__username', 'code']
+    readonly_fields = ('created_at',)
