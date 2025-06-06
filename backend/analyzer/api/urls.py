a90af390aadb225c48405627d34abdee13b8b05c
root <root@xzm.localdomain>
2025-06-06 19:48:07 +0000

    added pylint endpoint

--- backend/analyzer/api/urls.py
+++ backend/analyzer/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CodeSubmissionView
+from .views import CodeSubmissionView, CodeAnalysisView
 
 urlpatterns = [
-    path("submit-code/", CodeSubmissionView.as_view(), name='submit_code')
+    path("submit-code/", CodeSubmissionView.as_view(), name='submit_code'),
+    path('analyze/', CodeAnalysisView.as_view(), name='analyze'),
 ]
--- backend/analyzer/api/views.py
+++ backend/analyzer/api/views.py
@@ -3,6 +3,60 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from .serializers import CodeSubmissionSerializer
+from django.shortcuts import render
+from django.http import JsonResponse
+import subprocess
+import tempfile
+import os
+
+# Create your views here.
+class CodeAnalysisView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request):
+        code = request.data.get("code")
+        language = request.data.get("language")
+        
+        if language != "python":
+            return JsonResponse({"Only Python Supported"}, status=400)
+        
+        if not code:
+            return JsonResponse({'error': 'Code is required'}, status=400)
+        
+        try:
+            # Create Temporary File
+            with tempfile.NamedTemporaryFile(suffix=".py", delete=False) as temp_file:
+                temp_file.write(code.encode("utf-8"))
+                temp_file_path = temp_file.name
+             
+            # Run pylint    
+            result = subprocess.run(
+                ['pylint', temp_file_path, "--output-format=json"],
+                capture_output=True,
+                text=True
+            )
+            
+            # Remove temporary file
+            os.unlink(temp_file_path)
+            
+            # Parse pylint output
+            import json
+            pylint_output = json.loads(result.stdout) if result.stdout else []
+            analysis_results = [
+                {
+                    'line': issue['line'],
+                    'message': issue['message'],
+                    'type': issue['type'],
+                    'symbol': issue['symbol']
+                }
+                for issue in pylint_output
+            ]
+            
+            return JsonResponse({'results': analysis_results}, status=200)
+        
+        except Exception as e:
+            return JsonResponse({"error" : str(e)}, status=500)
+    
 
 class CodeSubmissionView(APIView):
     permission_classes = [IsAuthenticated]
